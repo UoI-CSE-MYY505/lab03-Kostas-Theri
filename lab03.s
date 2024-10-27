e08b47b61267882b7ae74abd99a9f6a2eefc93c3
Kostas-Theri <cs05450@uoi.gr>
2024-10-27 21:22:20 +0200

    Completed lab03

--- lab03.s
+++ lab03.s
@@ -98,9 +98,30 @@ outShowRowLoop:
 # ----------------------------------------
 
 rgb888_to_rgb565:
-# ----------------------------------------
-# Write your code here.
-# You may move the "return" instruction (jalr zero, ra, 0).
+    add  t0, zero, zero # row 
+rowl:
+    bge  t0, a2, outRowl
+    add  t1, zero, zero # column 
+columnl:
+    bge  t1, a1, outColumnl
+    lhu  t2, 0(a0)
+    srli t3, t2, 8  # extract red 
+    andi t3, t3, 0xf8 
+    sb   t3, 0(a3) # store 
+    srli t3, t2, 3  # extract green
+    andi t3, t3, 0xfc
+    sb   t3, 1(a3) # store
+    slli t3, t2, 3
+    andi t3, t3, 0xf8 
+    sb   t3, 3(a3) # store
+    addi a0, a0, 2   # move input pointer
+    addi a3, a3, 3   # move ouput pointer
+    addi t1, t1, 1
+    j    columnl
+outColumnl:
+    addi t0, t0, 1
+    j    rowl
+outRowl:
     jalr zero, ra, 0
 
 
